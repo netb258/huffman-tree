8ecb4c1c7855eeae8e3913bdefb6d43bd8428f9f
netb258 <netb258@abv.bg>
2026-06-21 15:12:55 +0300

    Modify project.clj for updated dependencies
    Updated dependencies and commented out the CIDER plugin.

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.14.0"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.nrepl "0.2.12"]] ;; Need this to please Cider:
+  ;; :plugins [[cider/cider-nrepl "0.49.0"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [org.clojure/tools.cli "1.4.256"]
+                 [metosin/malli "0.20.1"]]
   :main ^:skip-aot huffman-tree.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
